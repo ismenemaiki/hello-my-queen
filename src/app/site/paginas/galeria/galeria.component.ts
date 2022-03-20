dc89e88391e2afab2c1d16681c42deff366d8f73
MAIKI <ismenemaiki@gmail.com>
2022-03-20 16:16:44 -0300

    build: adicionado swiper.js

--- src/app/site/paginas/galeria/galeria.component.ts
+++ src/app/site/paginas/galeria/galeria.component.ts
@@ -1,5 +1,16 @@
 import { Component, OnInit } from '@angular/core';
+import Swiper, { Navigation, Pagination } from 'swiper';
+import SwiperCore, { Keyboard } from "swiper";
 
+SwiperCore.use([Navigation, Pagination, Keyboard]);
+
+const swiper = new Swiper('.swiper', {
+  modules: [ Navigation, Pagination ],
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+});
 @Component({
   selector: 'app-galeria',
   templateUrl: './galeria.component.html',
@@ -11,5 +22,4 @@ export class GaleriaComponent implements OnInit {
 
   ngOnInit(): void {
   }
-
 }
--- src/app/site/paginas/galeria/galeria.module.ts
+++ src/app/site/paginas/galeria/galeria.module.ts
@@ -2,9 +2,14 @@ import { GaleriaRoutingModule } from './galeria-routing.module';
 import { GaleriaComponent } from './galeria.component';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { SwiperModule } from 'swiper/angular';
 
 @NgModule({
   declarations: [ GaleriaComponent ],
-  imports: [ CommonModule, GaleriaRoutingModule ]
+  imports: [
+    CommonModule,
+    GaleriaRoutingModule,
+    SwiperModule
+  ]
 })
 export class GaleriaModule { }
